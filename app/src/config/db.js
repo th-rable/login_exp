bbb7020bdaca24dff263179ef83f2b1651198b3a
rable <hsjjace@gmail.com>
2023-11-01 14:21:15 +0900

    MySQL 연동

---
+++ app/src/config/db.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql2");
+const db_config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PSWORD,
+    database: process.env.DB_DATABASE,
+};
+
+var connection;
+
+function handleDisconnect(){
+    connection = mysql.createConnection(db_config);
+
+    connection.connect(function(err){
+        if(err){
+            console.log("error when connect to db");
+        }
+    });
+
+    module.exports = connection;
+
+    connection.on('error', function(err){
+        //console.log("db 재연결");
+        handleDisconnect();
+    })
+}
+
+handleDisconnect();
+
+
+
+
--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,27 @@ class User{
 
     async login(){
         const client = this.body;
-        const {id, psword} = await UserStorage.getUserInfo(client.id);
-        
-        if(id){
-            if(id === client.id && psword === client.psword){
-                return {success: true};
+
+        try {
+            const data = await UserStorage.getUserInfo(client.id);
+            if(data == undefined){
+                return {success: false, msg: "존재하지 않는 아이디입니다."};
             }
-            return {success: false, msg: "비밀번호가 틀렸습니다."};
+            const {id, psword} = data;
+
+
+            if(id){
+                if(id === client.id && psword === client.psword){
+                    return {success: true};
+                }
+                return {success: false, msg: "비밀번호가 틀렸습니다."};
+            }
+            return {success: false, msg: "존재하지 않는 아이디입니다."};
+        } catch (error) {
+            return {success: false, msg: error};
         }
-        return {success: false, msg: "존재하지 않는 아이디입니다."};
+
+        
     }
 
     async register(){
@@ -26,7 +38,8 @@ class User{
             const response = await UserStorage.save(client);
             return response;
         } catch (err) {
-            return{success: false, msg: err};
+
+            return{success: false, msg: "사용 불가능한 아이디입니다."};
         }
         
     }
